22b70061f7fbffdb60ee2a08a4fdc1877c71b749
QuentinLetort <quentin.letort@hotmail.fr>
2018-12-01 22:18:16 +0100

    add css+validation

--- src/com/Letort/web/jpa/Student.java
+++ src/com/Letort/web/jpa/Student.java
@@ -9,22 +9,25 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 @Entity
 @ManagedBean
 @RequestScoped
-public class Student implements Serializable{	
+public class Student implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)     
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int id;
-	@Column(name="first_name")
+	@Column(name = "first_name")
 	String firstName;
-	@Column(name="last_name")
+	@Column(name = "last_name")
 	String lastName;
 	String email;
+
 	public Student() {
 		super();
 	}
+
 	public Student(int id, String firstName, String lastName, String email) {
 		super();
 		this.id = id;
@@ -32,37 +35,46 @@ public class Student implements Serializable{
 		this.lastName = lastName;
 		this.email = email;
 	}
+
 	public Student(String firstName, String lastName, String email) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	@Override
 	public String toString() {
 		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
